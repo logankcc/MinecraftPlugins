4a3ab460f8af57b1329200374c697cff10b1d497
Logan Copeland <logankcc@gmail.com>
2024-07-11 20:52:51 -0700

    Updated plugins

--- hide-player-list-plugin/src/main/java/com/logan/HidePlayerListPlugin.java
+++ hide-player-list-plugin/src/main/java/com/logan/HidePlayerListPlugin.java
@@ -1,57 +1,22 @@
 package com.logan;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.Team;
 
-public class HidePlayerListPlugin extends JavaPlugin implements Listener {
+import static com.comphenix.protocol.PacketType.Play.Server.PLAYER_INFO;
 
-    private Team hiddenTeam;
+public class HidePlayerListPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
-
-        Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-        hiddenTeam = scoreboard.getTeam("hiddenTeam");
-
-        if (hiddenTeam == null) {
-            hiddenTeam = scoreboard.registerNewTeam("hiddenTeam");
-        }
-
-        hiddenTeam.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER);
-    }
-
-    @Override
-    public void onDisable() {
-        if (hiddenTeam != null) {
-            hiddenTeam.unregister();
-        }
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        new BukkitRunnable() {
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PLAYER_INFO) {
             @Override
-            public void run() {
-                hiddenTeam.addEntry(event.getPlayer().getName());
+            public void onPacketSending(PacketEvent event) {
+                event.setCancelled(true);
             }
-        }.runTaskLater(this, 10L);
-    }
-
-    @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                hiddenTeam.removeEntry(event.getPlayer().getName());
-            }
-        }.runTaskLater(this, 10L);
+        });
     }
 }
